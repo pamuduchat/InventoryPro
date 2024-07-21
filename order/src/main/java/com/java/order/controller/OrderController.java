56df757e80232797b4e65c49b0c56cd8795b1765
Pamudu Chathunka <119758027+pamuduchat@users.noreply.github.com>
2024-07-21 15:49:09 +0530

    Implement Kafka Integration

--- order/src/main/java/com/java/order/controller/OrderController.java
+++ order/src/main/java/com/java/order/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.java.order.controller;
 
+import com.abccompany.base.dto.OrderEventDTO;
 import com.java.order.common.OrderResponse;
 import com.java.order.dto.OrderDTO;
+import com.java.order.kafka.OrderProducer;
 import com.java.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,9 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private OrderProducer orderProducer;
+
     @GetMapping("/orders")
     public List<OrderDTO> getOrders() {
         return orderService.getAllOrders();
@@ -28,6 +33,10 @@ public class OrderController {
 
     @PostMapping("/orders")
     public OrderResponse saveOrder(@RequestBody OrderDTO orderDTO) {
+        OrderEventDTO orderEventDTO = new OrderEventDTO();
+        orderEventDTO.setMessage("Order is commited");
+        orderEventDTO.setStatus("pending");
+        orderProducer.sendMessage(orderEventDTO);
         return orderService.saveOrder(orderDTO);
     }
 
